478ff58b87e64e167f05680100fa3a9f84436c1c
daoswap002 <daoswap@daoswap.cc>
2021-07-10 11:53:29 +0800

    catalogue

--- migrations/3_deploy_ERC20DAT.js
+++
@@ -1,7 +0,0 @@
-const ERC20DAT = artifacts.require("ERC20DAT");
-
-module.exports = function (deployer) {
-  deployer.deploy(ERC20DAT,
-    // 构造函数的参数
-    "DAT : DAO Credential Token", "DAT", 18, 5000000);
-};
