e1fb0846c3fdc2ba5306dd6f77ef691122f019c2
daoswap002 <daoswap@daoswap.cc>
2021-12-31 00:38:04 +0800

    deploy period 3

--- migrations/6_deploy_CrowdsaleForRetail.js
+++ migrations/6_deploy_CrowdsaleForRetail.js
@@ -6,8 +6,8 @@ module.exports = function (deployer, network, accounts) {
   const beneficiaryToken = accounts[0]; // 收 DAT 的钱包地址
   const token = '0xc096332CAacF00319703558988aD03eC6586e704'; // 代币地址
   const deliverToken = accounts[0]; // 发送代币的钱包地址
-  const openingTime = 1633015800; // 众筹开始时间
-  const closingTime = 1640878200; // 众筹结束时间
+  const openingTime = 1640919600; // 众筹开始时间
+  const closingTime = 1656558000; // 众筹结束时间
   const cap = 5000000; // 众筹目标封顶数额，DAT
   // 部署合约
   deployer.deploy(CrowdsaleForRetail,
